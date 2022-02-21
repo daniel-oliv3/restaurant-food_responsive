91d14d3f3b7b8c84a82c868fe72adfc3c390e83d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-21 17:09:38 -0300

    Update Js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let searchForm = document.querySelector('.search-form-container');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 
@@ -9,16 +12,29 @@ let cart = document.querySelector('.shopping-cart-container');
 
 document.querySelector('#cart-btn').onclick = () => {
     cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let loginForm = document.querySelector('.login-form-container');
 
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
 }
