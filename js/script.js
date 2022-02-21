968e1e00f5278b93a170ffe078f6a15fd1dfe2cf
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-21 14:45:41 -0300

    Cart-btn active

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ let searchForm = document.querySelector('.search-form-container');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
+
+
+let cart = document.querySelector('.shopping-cart-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
+}
