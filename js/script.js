3bec60771bdc22a50dacf4f87987c5e81256052c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-21 17:51:48 -0300

    Update Js

--- js/script.js
+++ js/script.js
@@ -38,3 +38,12 @@ document.querySelector('#menu-btn').onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+document.querySelector('.home').onmousemove = (e) => {
+    let x = (window.innerWidth - e.pageX * 2) / 90;
+    let y = (window.innerHeight - e.pageY * 2) / 90;
+
+    document.querySelector('.home .home-parallax-img').style.transform =  `translateX(${y}px) translateY(${x}px)`;
+
+}
+
