71cba321c0b5006dc135d94cfbcba183e9dfe7fe
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-22 15:00:47 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Search Form/Formulario de Pesquisa */
 let searchForm = document.querySelector('.search-form-container');
 
 document.querySelector('#search-btn').onclick = () => {
@@ -8,6 +9,7 @@ document.querySelector('#search-btn').onclick = () => {
 }
 
 
+/*Shopping-cart section/Seção de Carrinho de Compras*/
 let cart = document.querySelector('.shopping-cart-container');
 
 document.querySelector('#cart-btn').onclick = () => {
@@ -17,6 +19,8 @@ document.querySelector('#cart-btn').onclick = () => {
     navbar.classList.remove('active');
 }
 
+
+/*Login Form/Formulario de Login*/
 let loginForm = document.querySelector('.login-form-container');
 
 document.querySelector('#login-btn').onclick = () => {
@@ -26,6 +30,7 @@ document.querySelector('#login-btn').onclick = () => {
     navbar.classList.remove('active');
 }
 
+/*Header navbar/Seção de cabeçalho /barra de navegação*/
 let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
@@ -39,17 +44,17 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+/*Efeito Imagem - Movimentação*/
 document.querySelector('.home').onmousemove = (e) => {
     let x = (window.innerWidth - e.pageX * 2) / 90;
     let y = (window.innerHeight - e.pageY * 2) / 90;
-
     document.querySelector('.home .home-parallax-img').style.transform =  `translateX(${y}px) translateY(${x}px)`;
-
 }
 
 document.querySelector('.home').onmouseleave = (e) => {
-
     document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
-
 }
 
+
+
+/**/
